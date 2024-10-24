a2229114193537478009717c23025ec416981c6d
llanjo01 <llanjo01@gettysburg.edu>
2024-10-23 22:14:58 -0400

    backend update

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import {
 	getUserByEmail,
 	getUsers,
 	insertData,
+	getPasswordByEmail
 } from "./database.js";
 
 const app = express();
@@ -21,6 +22,12 @@ app.get("/user/:email", async (req, res) => {
 	const user = await getUserByEmail(email);
 	res.send(user);
 });
+
+app.get("/password/:email", async (req, res) => {
+	const email = req.params.email;
+	const password = await getPasswordByEmail(email);
+	res.send(password);
+})
 
 // post's http
 app.get("/posts", async (req, res) => {
